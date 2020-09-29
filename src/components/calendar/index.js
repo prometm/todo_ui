d12268eebd48cd68b66d55d61092694545935ad2
prometm <prometm@i.ua>
2020-09-29 22:30:22 +0300

    Todo 2

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -8,7 +8,7 @@ import './index.css';
 export default class Calendar extends React.Component {
     static defaultProps = {
         date: new Date(),
-        years: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020],
+        years: [2020, 2021, 2022, 2023],
         monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
         weekDayNames: ['Пн', 'Вт', 'Ср', 'Чт' , 'Пт', 'Сб', 'Вс'],
         onChange: Function.prototype
